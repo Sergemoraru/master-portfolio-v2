cb58bddcea601560c16dae98020008e37546c59d
Serge Moraru <sergemoraru@gmail.com>
2023-07-25 10:52:36 -0400

    created contact form and fixed few bugs

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
+
 const NavBar = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
       const isTop = scrollTop === 0;
       setIsScrolled(!isTop);
     };
@@ -17,40 +18,27 @@ const NavBar = () => {
   }, []);
 
 
-  const homeButtonClick = () => {
-    const homeSection = document.getElementById('homeSection');
-    if (homeSection) {
-        window.scrollTo({
-            top: homeSection.offsetTop,
-            behavior: 'smooth',
-        });
-    }
-    };
-
 
   return (
     <nav
-      className={`fixed top-0 w-full bg-white transition-all duration-300 ${
-        isScrolled ? 'shadow-lg' : ''
+      className={`fixed top-0 w-full bg-white ${
+        isScrolled ? 'shadow-lg bg-slate-200' : 'transition-all duration-500 ease-in-out bg-transparent'
       }`}
       style={{ zIndex: isScrolled ? 999 : 'auto' }}
     >
-      <div className="flex flex-row justify-center items-center relative">
-        <div className="flex flex-row justify-center items-center">
-          <button className="text-2xl font-bold text-slate-700 hover:text-sky-500 relative p-10">Home</button>
+      <div className="flex flex-row justify-end items-center p-10">
+        <div>
+          <button className="text-2xl font-bold text-slate-700 hover:text-sky-500 relative p-2 pr-10">Projects</button>
         </div>
         <div className="flex flex-row justify-center items-center">
-          <button className="text-2xl font-bold text-slate-700 hover:text-sky-500 relative p-10">About</button>
+          <button className="text-2xl font-bold text-slate-700 hover:text-sky-500 relative p-2 pr-10">Contact</button>
         </div>
-        <div className="flex flex-row justify-center items-center">
-          <button className="text-2xl font-bold text-slate-700 hover:text-sky-500 relative p-10">Projects</button>
+        <div className="border-slate-500 border-solid border-4 rounded-xl">
+          <button className="text-2xl font-bold text-slate-700 hover:text-sky-500 relative p-2 pl-5 pr-5">Resume</button>
         </div>
-        <div className="flex flex-row justify-center items-center">
-          <button className="text-2xl font-bold text-slate-700 hover:text-sky-500 relative p-10">Resume</button>
-        </div>
-        <div className="flex flex-row justify-center items-center">
-          <button className="text-2xl font-bold text-slate-700 hover:text-sky-500 relative p-10">Contact</button>
-        </div>
+      </div>
+      <div>
+        <h1 className="flex flex-row justify-start p-10 text-4xl font-bold font-cursive text-center text-sky-700 absolute top-2 ">Sergey Moraru</h1>
       </div>
     </nav>
   );
