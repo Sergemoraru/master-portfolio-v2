1245c8ebb624483d129d56af8044fe322ee45bb2
Serge Moraru <sergemoraru@gmail.com>
2023-08-13 20:54:32 -0400

    added bg for hero, adjusted navbar, icons added

--- components/mySkills.tsx
+++ components/mySkills.tsx
@@ -4,48 +4,135 @@ import React from "react"
 
 
 const  MySkills = () => {
-    return(
-        <div>
-            <div className="flex flex-col pt-32">
-               <h1 className='flex flex-row mt-48 justify-center p-10 text-5xl font-bold text-center text-slate-500'>My 
-               <script className="flex flex-row relative text-5xl font-bold text-sky-500 left-3"> Skills</script></h1>
-            </div>
-            <hr className="relative bottom-5"></hr>
-            <div className="flex flex-row text-center justify-center">
-                <div>
-                    <Image src="/next.svg" alt="next" width={100} height={100} 
-                    className=" flex flex-row relative justify-center top-14 right-10" />
-                </div>
-                <div>
-                    <Image src="/react-2.svg" alt="PrimarySkills" width={100} height={100} 
-                    className=" flex flex-row relative justify-center top-14 " />
-                </div>
-                <div>
-                    <Image src="/typescript.svg" alt="PrimarySkills" width={100} height={100} 
-                    className=" flex flex-row relative justify-center top-14 left-10" />
-                </div>
-                <div>
-                    <Image src="/logo-javascript.svg" alt="PrimarySkills" width={100} height={100} 
-                    className=" flex flex-row relative justify-center top-14 left-20" />
-                </div>
-                <div className="">
-                    <Image src="/nodejs-icon.svg" alt="PrimarySkills" width={50} height={50} 
-                    className=" flex flex-row relative justify-center top-14 left-32" /> 
-                    <p className="flex flex-row relative text-right justify-center left-52 top-3 font-semibold fon t-mono text-xl">Node</p>
-                </div>
-            </div>
-            <div className="flex flex-row text-center justify-center pt-8">
-                <Image src="/prisma-2.svg" alt="next" width={100} height={100} 
-                className=" flex flex-row relative justify-center top-14 right-10" />
-                <Image src="/tailwindcss.svg" alt="PrimarySkills" width={100} height={100} 
-                className=" flex flex-row relative justify-center top-14 " />
-                <Image src="/html-1.svg" alt="PrimarySkills" width={100} height={100} 
-                className=" flex flex-row relative justify-center top-14 left-10" />
-                <Image src="/css-3.svg" alt="PrimarySkills" width={100} height={100} 
-                className=" flex flex-row relative justify-center top-14 left-20" />
-            </div>
+    return (
+      <div>
+        <div className="flex flex-col pt-32">
+          <h1 className="flex flex-row mt-48 justify-center p-10 text-5xl font-bold text-center text-slate-700">
+            What I know
+          </h1>
+          <h3 className="flex justify-center font-semibold text-slate-600 text-xl">
+            Eager to dive into every corner of technology to create dynamic and
+            impactful solutions.
+          </h3>
         </div>
-    )
+        <div>
+          <div className="relative ml-20">
+            <div className="relative inline-grid grid-cols-4 ">
+              <div className="relative inline-grid grid-cols-2 grid-rows-3 space-x-5 space-y-10">
+                <h2 className="relative pb-20">Front End</h2>
+                <Image
+                  src="/next.svg"
+                  alt="next"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <Image
+                  src="/react-2.svg"
+                  alt="react"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <Image
+                  src="/typescript.svg"
+                  alt="typescript"
+                  width={100}
+                  height={100}
+                  className=" "
+                />
+                <Image
+                  src="/logo-javascript.svg"
+                  alt="javascript"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <Image
+                  src="/tailwindcss.svg"
+                  alt="tailwindcss"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <Image
+                  src="/html-1.svg"
+                  alt="html"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <Image
+                  src="/css-3.svg"
+                  alt="css"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+              </div>
+              <div>
+                <h2>Back End</h2>
+                <Image
+                  src="/nodejs-icon.svg"
+                  alt="nodejs"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+              </div>
+              <div>
+                <h2>Database</h2>
+                <Image
+                  src="/prisma-2.svg"
+                  alt="prisma"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <Image
+                  src="/mongodb-icon-1.svg"
+                  alt="mongodb"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <Image
+                  src="/postgresql.svg"
+                  alt="sql"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+              </div>
+              <div>
+                <h2>Languages</h2>
+                <Image
+                  src="/united-states-of-america.svg"
+                  alt="english"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <Image
+                  src="/flag-of-russia.svg"
+                  alt="russian"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <Image
+                  src="/ukrainef.svg"
+                  alt="ukrainian"
+                  width={100}
+                  height={100}
+                  className=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default MySkills
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
 import NavBar from '@/components/navbar';
 import TypeWriterText from '@/components/typeWriterText';
@@ -8,49 +9,76 @@ import Projects from '@/components/projects';
 import Contact from '@/components/contact';
 import Footer from '@/components/footer';
 import MySkills from '@/components/mySkills';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div id='main-container'>
+    <div id='main-container' className='bg-img'>
+      <div>
+        <img
+          src={"/topography1.png"}
+          alt={""}
+          className="absolute top-0 left-0 -z-10"
+          style={{ backgroundSize: "cover", backgroundRepeat: "repeat-y" }}
+        />
         <div>
           <NavBar />
         </div>
-          <div className='flex flex-row justify-center items-center relative top-5'>
-            <div className='flex justify-center items-center relative mt-20 pt-11 top-20'>
-              <div className='flex flex-row justify-center items-center relative top-16' style={{ height: '100px' }}>
-                <TypeWriterText />
-              </div>
+        <div className="flex flex-row justify-center items-center relative top-5">
+          <div className="flex justify-center items-center relative mt-20 pt-11 top-20">
+            <div
+              className="flex flex-row justify-center items-center relative top-16"
+              style={{ height: "100px" }}
+            >
+              <TypeWriterText />
             </div>
           </div>
-          <div className='flex flex-row justify-center items-center top-52 pt-56 p-10 mb-14 static'>
-              <Link href='https://github.com' className='flex flex-row justify-center items-center p-16 top-24 relative text-slate-600 hover:text-sky-500'>
-                  <FontAwesomeIcon icon={faGithub} size='2xl' />
-              </Link>
-              <Link href='https://twitter.com' className='flex flex-row justify-center items-center p-16 top-24 relative text-slate-600 hover:text-sky-500'>
-                  <FontAwesomeIcon icon={faTwitter} size='2xl' />
-              </Link>
-              <Link href='https://linkedin.com' className='flex flex-row justify-center items-center p-16 top-24 relative text-slate-600 hover:text-sky-500'>
-                  <FontAwesomeIcon icon={faLinkedin} size='2xl' />
-              </Link>
-              <Link href='https://discord.com' className='flex flex-row justify-center items-center p-16 top-24 relative text-slate-600 hover:text-sky-500'>
-                  <FontAwesomeIcon icon={faDiscord} size='2xl' />
-              </Link>
         </div>
-      <div className='flex flex-col justify-center items-center relative top-56' id=''>
+        <div className="flex flex-row justify-center items-center top-52 pt-56 p-10 mb-14 static">
+          <Link
+            href="https://github.com"
+            className="flex flex-row justify-center items-center p-16 top-24 relative text-slate-600 hover:text-sky-500"
+          >
+            <FontAwesomeIcon icon={faGithub} size="2xl" />
+          </Link>
+          <Link
+            href="https://twitter.com"
+            className="flex flex-row justify-center items-center p-16 top-24 relative text-slate-600 hover:text-sky-500"
+          >
+            <FontAwesomeIcon icon={faTwitter} size="2xl" />
+          </Link>
+          <Link
+            href="https://linkedin.com"
+            className="flex flex-row justify-center items-center p-16 top-24 relative text-slate-600 hover:text-sky-500"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+          </Link>
+          <Link
+            href="https://discord.com"
+            className="flex flex-row justify-center items-center p-16 top-24 relative text-slate-600 hover:text-sky-500"
+          >
+            <FontAwesomeIcon icon={faDiscord} size="2xl" />
+          </Link>
+        </div>
+        <div
+          className="flex flex-col justify-center items-center relative pt-20"
+          id="#about-me"
+        >
           <AboutMe />
         </div>
-            <div>
-              <MySkills />
-            </div>
-            <div>
-              <Projects />
-            </div>
-            <div>
-              <Contact />
-            </div>
-            <div>
-              <Footer />
-            </div>
+        <div>
+          <MySkills />
+        </div>
+        <div>
+          <Projects />
+        </div>
+        <div>
+          <Contact />
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
